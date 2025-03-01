e037f1a29e8cac077772d766a50295c4d45aac02
ELCRND <jwhh2a@gmail.com>
2025-03-01 21:59:49 +0300

    modified:   index.html 	modified:   main.js 	deleted:    public/img/Group (7).svg 	modified:   styles.css

--- main.js
+++ main.js
@@ -1,49 +1,72 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const slider = document.querySelector(".slider");
-  const slides = document.querySelectorAll(".slide");
-  slider.style.transition = `transform ${sliderSpeed}ms ease-out`;
+const menu = document.querySelector(".main-nav");
+const menuBtn = document.querySelector(".header-menu-btn");
 
-  const sliderSpeed = 700; // ms
-  let currentSlide = 0;
-  let isScrolling = false;
-  let startY = 0; // Начальная позиция свайпа
-  let endY = 0; // Конечная позиция свайпа
-  const swipeThreshold = 50; // Порог свайпа
+menuBtn.addEventListener("click", () => {
+  menu.classList.toggle("open");
+  menuBtn.classList.toggle("open");
+});
 
-  function goToSlide(index) {
-    if (index < 0 || index >= slides.length) return;
-    currentSlide = index;
-    const offset = -currentSlide * 100;
-    slider.style.transform = `translateY(${offset}vh)`;
+const navLinks = document.querySelectorAll(".nav-item a");
+const slider = document.querySelector(".slider");
+const slides = document.querySelectorAll(".slide");
+const sliderSpeed = 700; // ms
+slider.style.transition = `transform ${sliderSpeed}ms ease-out`;
+
+let currentSlide = 0;
+let isScrolling = false;
+let startY = 0; // Начальная позиция свайпа
+let endY = 0; // Конечная позиция свайпа
+const swipeThreshold = 50; // Порог свайпа
+
+function goToSlide(index) {
+  if (index < 0 || index >= slides.length) return;
+  currentSlide = index;
+  const offset = -currentSlide * 100;
+  slider.style.transform = `translateY(${offset}vh)`;
+
+  navLinks.forEach((link) => {
+    link.classList.toggle(
+      "active",
+      link.getAttribute("data-slide") == currentSlide
+    );
+  });
+}
+
+// Обработчик скролла
+window.addEventListener("wheel", (e) => {
+  if (isScrolling) return; // debounce
+  isScrolling = true;
+
+  e.deltaY > 0 ? goToSlide(currentSlide + 1) : goToSlide(currentSlide - 1);
+
+  setTimeout(() => {
+    isScrolling = false;
+  }, 500);
+});
+
+slider.addEventListener("touchstart", (e) => {
+  startY = e.touches[0].clientY;
+});
+
+slider.addEventListener("touchmove", (e) => {
+  endY = e.touches[0].clientY;
+});
+
+slider.addEventListener("touchend", () => {
+  const deltaY = startY - endY;
+
+  if (deltaY > swipeThreshold) {
+    goToSlide(currentSlide + 1);
+  } else if (deltaY < -swipeThreshold) {
+    goToSlide(currentSlide - 1);
   }
+});
 
-  // Обработчик скролла
-  window.addEventListener("wheel", (e) => {
-    if (isScrolling) return; // debounce
-    isScrolling = true;
-
-    e.deltaY > 0 ? goToSlide(currentSlide + 1) : goToSlide(currentSlide - 1);
-
-    setTimeout(() => {
-      isScrolling = false;
-    }, 500);
-  });
-
-  slider.addEventListener("touchstart", (e) => {
-    startY = e.touches[0].clientY;
-  });
-
-  slider.addEventListener("touchmove", (e) => {
-    endY = e.touches[0].clientY;
-  });
-
-  slider.addEventListener("touchend", () => {
-    const deltaY = startY - endY;
-
-    if (deltaY > swipeThreshold) {
-      goToSlide(currentSlide + 1);
-    } else if (deltaY < -swipeThreshold) {
-      goToSlide(currentSlide - 1);
-    }
+// Обработчики для навигационных ссылок
+navLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const slideNum = +link.getAttribute("data-slide");
+    goToSlide(slideNum);
   });
 });
